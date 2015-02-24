7a809dd100c63c3ed933492c8cbb5f37b4c02e60
AllAboutEE <miguel@allaboutee.com>
2015-02-24 15:27:33 -0600

    Added functions, and function definitions.

--- AllAboutEE_ESP8266.cpp
+++ AllAboutEE_ESP8266.cpp
@@ -1,36 +1,58 @@
 #include <AllAboutEE_ESP8266.h>
 
+// TODO: add command verification on all functions
+
+AllAboutEE::ESP8266::ESP8266(Stream *s,Stream *d,int8_t rp):stream(s),debug(d),resetPin(rp)
+{
+
+}
+
+
 String AllAboutEE::ESP8266::getIPAddress()
 {
-    return "192.168.0.1";
+    char command[] = "AT+CIFSR\r\n";
+    return write(command,sizeof(command)/sizeof(command[0]),1000);
 }
 
-String AllAboutEE::ESP8266::sendCommand(String command, unsigned long timeout)
+bool AllAboutEE::ESP8266::cwJap(const String SSID, const String PASSWORD)
 {
-    return "FAIL";
-}
 
-bool AllAboutEE::ESP8266::ack(String m)
-{
+    const int commandLength = 16+SSID.length()+PASSWORD.length();
+
+    char command[100] = "AT+CWJAP=\""; // AT+CWJAP="ssid","pass"\r\n
+
+    char s[SSID.length()];
+    SSID.toCharArray(s,SSID.length()+1);
+    strcat(command,s);
+    strcat(command,"\",\"");
+
+    char p[PASSWORD.length()];
+    PASSWORD.toCharArray(p,PASSWORD.length()+1);
+    strcat(command,p);
+    strcat(command,"\"\r\n");
+    write(command,commandLength,5000);
+
     return false;
 }
 
-bool AllAboutEE::ESP8266::joinAP(const String SSID, const String PASSWORD)
-{
-    return false;
-}
 
 bool AllAboutEE::ESP8266::cwMode(unsigned int mode)
 {
+    char command[13]="AT+CWMODE="; // AT+CWMODE=x\r\n
+    char m[1];
+
     switch(mode)
     {
     case 1:
-        break;
     case 2:
-        break;
     case 3:
+        sprintf(m,"%d",mode);
+        strcat(command,m);
+        strcat(command,"\r\n");
+        write(command,sizeof(command)/sizeof(command[0]),1000);
         break;
     default:
+        return false;
         break;
     }
     
@@ -48,11 +70,19 @@ bool AllAboutEE::ESP8266::cipSend(unsigned int connectionId, String data)
     return false;
 }
 
-bool AllAboutEE::ESP8266::cipClose()
+bool AllAboutEE::ESP8266::cipClose(unsigned int connectionId)
 {
+    char command[15]="AT+CIPCLOSE="; // AT+CIPCLOSE=x\r\n
+    char connection[1];
+
+    sprintf(connection,"%d",connectionId);
+    strcat(command,connection);
+    strcat(command,"\r\n");
+    write(command,sizeof(command)/sizeof(command[0]),1000);
 
     return false;
 }
+
 
 bool AllAboutEE::ESP8266::cipServer(bool state, unsigned int port)
 {
@@ -61,17 +91,90 @@ bool AllAboutEE::ESP8266::cipServer(bool state, unsigned int port)
 
 bool AllAboutEE::ESP8266::cipMux(bool state)
 {
+    char command[13] = "AT+CIPMUX=";
+    char s[1];
 
+    if(state)
+    {
+        sprintf(s,"%d",1);
+    }
+    else
+    {
+        sprintf(s,"%d",1);
+    }
+
+    strcat(command,s);
+    strcat(command,"\r\n");
+    write(command,sizeof(command)/sizeof(command[0]),1000);
+
+    return false;
+}
+
+String AllAboutEE::ESP8266::cipStatus()
+{
+    return "FAIL";
+}
+
+bool AllAboutEE::ESP8266::cwQap()
+{
+    char command[] = "AT+CWQAP\r\n";
+    return  write(command,sizeof(command)/sizeof(command[0]),5000);
+    return false;
+}
+
+String AllAboutEE::ESP8266::cwLap()
+{
+    char command[] = "AT+CWLAP\r\n";
+    return  write(command,sizeof(command)/sizeof(command[0]),5000);
 }
 
 bool AllAboutEE::ESP8266::hardwareReset()
 {
-    return false;
+    if(resetPin < 0) return true;
+
+    digitalWrite(resetPin, LOW);
+    pinMode(resetPin, OUTPUT); // Open drain; reset -> GND
+    delay(10);                  // Hold a moment
+    pinMode(resetPin, INPUT);  // Back to high-impedance pin state
+
+    return stream->find("Version");
 }
+
 
 bool AllAboutEE::ESP8266::softwareReset()
 {
+    char command[] = "AT+RST\r\n";
+    write(command,sizeof(command)/sizeof(command[0]),2000);
     return false;
 }
 
+String AllAboutEE::ESP8266::write(char* data, int dataSize, unsigned long timeoutMs)
+{
+    String response = "";
+    
+    stream->write(data,dataSize);
 
+    unsigned long time = millis();
+
+    while((time+timeoutMs)>millis())
+    {
+        while(stream->available())
+        {
+            char c = stream->read();
+            response +=c;
+        }
+    }
+
+    if(response.length()==0)
+    {
+       // this command did not respond or does not have a response
+       response = "NULL";
+    }
+
+    if(debug){
+        debug->print(response);
+    }
+
+
+    return response;
+}
--- AllAboutEE_ESP8266.h
+++ AllAboutEE_ESP8266.h
@@ -1,4 +1,3 @@
-
 #ifndef _ALLABOUTEE_ESP8266_H
 #define _ALLABOUTEE_ESP8266_H
 
@@ -6,21 +5,157 @@
 
 namespace AllAboutEE
 {
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015) 
+     *  
+     * @details The "ESP8266" can be used to interface with the 
+     *        ESP8266 module via UART. The class has functions which
+     *        can send the ESP8266 commands, and display their
+     *        response.
+     */
     class ESP8266
     {
+    public:
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param s The ESP UART/serial object
+     * @param d The serial debug object i.e. the serial object for 
+     *          the debug console
+     * @param rp The ESP's reset pin
+     */
+        ESP8266(Stream *s,Stream *d = NULL, int8_t rp = -1);
 
-        String getIPAddress(),
-               sendCommand(String command, unsigned long timeout);
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @return String The IP address assigned to the ESP8266 by the 
+     *         access point.
+     */
+        String getIPAddress();
 
-        bool    ack(String m),
-                joinAP(const String SSID, const String PASSWORD),
-                cwMode(unsigned int mode),
-                cipSend(unsigned int connectionId, String data),
-                cipClose(),
-                cipServer(bool state, unsigned int port),
-                cipMux(bool state),
-                hardwareReset(),
-                softwareReset();
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param SSID The access point's SSID (network name)
+     * @param PASSWORD The access point's password/passphrase
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool cwJap(const String SSID,const String PASSWORD);
+
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param mode 1 = station, 2 = access point, 3 = both
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool cwMode(unsigned int mode);
+
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param connectionId The ID of the connection you wish to send 
+     *                     the data to
+     * @param data The data to send
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool cipSend(unsigned int connectionId, String data);
+
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param connectionId The ID of the connection you wish to 
+     *                     close.
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool cipClose(unsigned int connectionId);
+
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param state Turn the server on/off (true/false) 
+     * @param port The server port, where it will receive 
+     *             connections
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool cipServer(bool state, unsigned int port);
+
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param state Enable multiple connections? yes/no (true/false)
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool cipMux(bool state);
+     
+
+        String cipStatus(); 
+
+        bool cwQap();
+
+        String cwLap();
+
+     /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool hardwareReset();
+
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @return bool Command execution success/failure (true/false)
+     */
+        bool softwareReset();
+
+    private:
+    /**
+     * 
+     * 
+     * @author Miguel (2/24/2015)
+     * 
+     * @param data The data bytes to send to the ESP8266
+     * @param dataSize Size of the data array
+     * @param timeoutMs time to wait for a response from the ESP8266
+     *                  (if one is expected)
+     * 
+     * @return String The response from the ESP8266
+     */
+        String write(char* data, int dataSize, unsigned long timeoutMs);
+
+        Stream *stream,
+               *debug;
+
+        int8_t resetPin;
         
     };
 }
